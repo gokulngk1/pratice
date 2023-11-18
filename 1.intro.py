1defbd920f8551308855e1594807ebe2f7e4051b
Gokul <107466191+gokulngk1@users.noreply.github.com>
2023-11-18 15:39:11 +0530

    paratic

---
+++ 1.intro.py
@@ -0,0 +1,43 @@
+#    ex1
+
+# list intro
+
+# numbers = [1,2,25,124,657]
+# names = ["gokul","tharani","emily"]
+# print(names)
+
+
+# ex2
+
+# likes = [["tv", "gaming"],["prizza", "pastha"]]
+
+# print(likes)
+
+# amamil = ["cats"]*10
+# print(amamil)
+
+
+# list metho******************
+# ex1
+# num = list(range(100))
+# print(num)
+# ex2
+
+# randam_name =list("gokul")
+# print(randam_name)
+
+
+# print(len(randam_name))
+
+
+# # accessing list 
+collection =[28,"m",32,"h,","gokul",223]
+# # collection[5]= "python"
+print(collection)
+
+
+# # range
+
+# print(collection[0:2])
+# print(collection[0:3])
+# print(collection[0])
